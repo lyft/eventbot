84eeb1046e59eb8b95a0447c826c86af390adf58
Ryan Lane <rlane@lyft.com>
2019-11-25 15:50:23 -0800

    Add github actions (#2)
    * Add github actions
    
    * Remove max complexity and fix unused import issue
    
    * Add omnibot receiver library
    
    * Bump omnibot-receiver dep
    
    * Manifest for version, and fixes for pypi
    
    * Update receiver version
    
    * Bump receiver version again
    
    * Fix wsgi test
    
    * Remove error handler test mocking healthcheck
    
    * Lower coverage

--- tests/unit/eventbot/wsgi_test.py
+++ tests/unit/eventbot/wsgi_test.py
@@ -1,20 +1,7 @@
 from eventbot.wsgi import app
-from unittest.mock import patch
-import json
 
 
 def test_healthcheck():
     response = app.test_client().get('/healthcheck')
     assert response.status_code == 200
     assert response.data.decode('ascii') == 'OK'
-
-
-@patch('eventbot.routes.views.do_healthcheck')
-def test_handle_error(healthcheck_fn):
-    healthcheck_fn.side_effect = Exception('testException')
-
-    response = app.test_client().get('/healthcheck')
-
-    assert response.status_code == 500
-    json_data = json.loads(response.data)
-    assert json_data['error'] == 'Internal server error'
